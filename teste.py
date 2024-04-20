8d3746798aab55cb1f15b187d6f27e8983ef9c04
adna silva <adnaoliveirasilva09@gmail.com>
2024-04-20 14:52:46 -0300

    new: criação da venv e adição dos pacotes no requirements

--- teste.py
+++ teste.py
@@ -1,9 +1,11 @@
 from selenium.webdriver import Firefox
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
 
-url = 'https://duckduckgo.com/'
+url = 'https://google.com/'
 browser = Firefox()
 browser.get(url)
