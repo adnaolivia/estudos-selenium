126774f95760028c045b023e1bd131079b5d1a6a
adna silva <adnaoliveirasilva09@gmail.com>
2024-04-20 18:11:32 -0300

    chore: organizando o codigo com classe e metodos

--- teste.py
+++ teste.py
@@ -3,9 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-options = webdriver.FirefoxOptions()
-driver = webdriver.Firefox(options=options)
+driver = webdriver() # instancia o driver
+browser = Firefox() # instancia o firefox
 
-url = 'https://google.com/'
-browser = Firefox()
-browser.get(url)
+class teste_selenium():
+
+    barra_pesquisa_locator = driver.find_element(By.CSS_SELECTOR,"#APjFqb") # retornou NoSuchElement ://
+
+    def abrir_firefox(self):
+        url = 'https://google.com/'
+        browser.get(url)
+    
+    def fazer_pesquisa(self):
+        fazer_pesquisa = driver.find_element(*self.barra_pesquisa_locator)
+        fazer_pesquisa.send_keys("youtube" + Keys.ENTER)
